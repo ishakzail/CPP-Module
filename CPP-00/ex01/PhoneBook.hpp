711558ff567635da8a07b6a80f9411de6aaec756
ishakzail <ishak.zail.iz@gmail.com>
2022-08-12 11:46:59 +0100

    change

--- CPP-00/ex01/PhoneBook.hpp
+++ CPP-00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: izail <izail@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 02:11:10 by izail             #+#    #+#             */
-/*   Updated: 2022/08/12 04:58:53 by izail            ###   ########.fr       */
+/*   Updated: 2022/08/12 10:17:11 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 class PhoneBook
 {
     private :
-        Contact _Contact[8];
+        Contact _Contacts[8];
         int     nbr_contacts;
-    
+        int     idx_contact;
     public :
         PhoneBook(void){};
         ~PhoneBook(){};
        
        void     addContact();
-       void     printContact(Contact c);
+       void     printContact();
        
        // string  searchContact(); 
 };
