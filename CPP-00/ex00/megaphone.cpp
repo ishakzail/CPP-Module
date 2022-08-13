3246c4dd927d5dd9286ff845e55c33141fe65f55
ishakzail <ishak.zail.iz@gmail.com>
2022-08-13 16:45:10 +0100

    update (delete using namespace std)

--- CPP-00/ex00/megaphone.cpp
+++ CPP-00/ex00/megaphone.cpp
@@ -6,13 +6,11 @@
 /*   By: izail <izail@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 15:53:11 by izail             #+#    #+#             */
-/*   Updated: 2022/08/09 21:44:52 by izail            ###   ########.fr       */
+/*   Updated: 2022/08/13 16:44:48 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<iostream>
-
-using namespace std;
 
 char    *capitalize(char *str)
 {
@@ -35,10 +33,12 @@ int main(int ac, char **av)
 
     i = 0;
     if (ac == 1)
-        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     else if (ac > 1)
         while (av[++i])
-            cout << capitalize(av[i]);
-    cout << endl;
+            std::cout << capitalize(av[i]);
+
+            
+    std::cout << std::endl;
     return (0);
 }
