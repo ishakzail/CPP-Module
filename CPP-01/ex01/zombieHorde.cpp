ec8560352bed3ea5ca7fdd6a2d2fa9bb08b6446d
ishakzail <ishak.zail.iz@gmail.com>
2022-09-30 08:27:55 +0100

    protect allocation and number of zombies

--- CPP-01/ex01/zombieHorde.cpp
+++ CPP-01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: izail <izail@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 09:57:19 by izail             #+#    #+#             */
-/*   Updated: 2022/09/25 11:45:49 by izail            ###   ########.fr       */
+/*   Updated: 2022/09/28 08:58:01 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,11 @@
 Zombie* zombieHorde(int n, string name)
 {
     int i = -1;
+    if (n <= 0)
+        return (NULL);
     Zombie *Zombies = new Zombie[n];
-    
+    if (!Zombies)
+        return (NULL);
     while (++i < n)
     {
         Zombies[i].setName(name);
