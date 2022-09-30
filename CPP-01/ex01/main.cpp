594dac2fdee9a2c7b4c69655122973bedd752143
ishakzail <ishak.zail.iz@gmail.com>
2022-09-30 08:27:37 +0100

    update

--- CPP-01/ex01/main.cpp
+++ CPP-01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: izail <izail@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 09:57:10 by izail             #+#    #+#             */
-/*   Updated: 2022/09/25 11:46:05 by izail            ###   ########.fr       */
+/*   Updated: 2022/09/28 09:02:25 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@
 int main(void)
 {
     Zombie *Zs; 
-    // std::cout << "Hello" ;
-    Zs = zombieHorde(9, "Waaaa Baaaarcaaaa");
-    // // while ()
+    Zs = zombieHorde(10, "Waaaa Baaaarcaaaa");
     delete [] Zs;
     return (0);
 }
