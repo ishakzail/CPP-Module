070dd0a2450be1f446579d6b8bc4649990baaa31
ishakzail <ishak.zail.iz@gmail.com>
2022-09-30 08:27:06 +0100

    protect allocation

--- CPP-01/ex00/newZombie.cpp
+++ CPP-01/ex00/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: izail <izail@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 08:36:26 by izail             #+#    #+#             */
-/*   Updated: 2022/09/25 08:47:42 by izail            ###   ########.fr       */
+/*   Updated: 2022/09/28 08:46:02 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@ Zombie* newZombie(string name)
 {
     Zombie *zomb;
     zomb = new Zombie(name);
+    if(!zomb)
+        return (NULL);
     return (zomb);
 }
