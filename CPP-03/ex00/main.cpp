7e0a015da87461b9485d33f4d5d2a6cef3cd128c
ishakzail <ishak.zail.iz@gmail.com>
2022-10-17 17:50:04 +0100

    updated main.cpp

--- CPP-03/ex00/main.cpp
+++ CPP-03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: izail <izail@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 11:36:43 by izail             #+#    #+#             */
-/*   Updated: 2022/10/14 08:47:21 by izail            ###   ########.fr       */
+/*   Updated: 2022/10/17 09:00:43 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,19 @@
 int main(void)
 {
     ClapTrap fighter1("Ishak");
-	ClapTrap fighter2;
-	fighter2 = ClapTrap("Khobza");
+	ClapTrap fighter2("Messi");
 
 	std::cout << std::endl;
 	std::cout << "[====================Round 1====================]" << std::endl;
 
-	fighter1.attack("barca");
+	fighter1.attack("khobza");
 	fighter1.takeDamage(11);
 	fighter1.beRepaired(5);
 
 	std::cout << std::endl;
 	std::cout << "[====================Round 2====================]" << std::endl;
 
-	fighter2.attack("piwpiw");
+	fighter2.attack("madrid");
 	fighter2.takeDamage(4);
 	fighter2.beRepaired(2);
 	fighter2.takeDamage(8);
