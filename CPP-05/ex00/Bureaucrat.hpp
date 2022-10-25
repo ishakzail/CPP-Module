badb04d3aca0804e66e2b96b8ae1ce0db34d6ab4
ishakzail <ishak.zail.iz@gmail.com>
2022-10-25 16:15:41 +0100

    CP05- ex00

---
+++ CPP-05/ex00/Bureaucrat.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: izail <izail@student.1337.ma>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 20:33:18 by izail             #+#    #+#             */
+/*   Updated: 2022/10/25 15:06:33 by izail            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef CLASS_BUREAUCRAT_H
+# define CLASS_BUREAUCRAT_H
+
+#include <iostream>
+#include <exception>
+
+class Bureaucrat
+{
+    private :
+        const std::string name;
+        int grade;
+    public :
+        Bureaucrat();
+        Bureaucrat(const std::string _name, int _grade);
+        Bureaucrat(const Bureaucrat &obj);
+        Bureaucrat &operator=(const Bureaucrat &obj);
+        std::string getName() const;
+        void   setName(const std::string _name);
+        int getGrade() const;
+        void setGrade(int _grade);
+        void incrementBureacrat();
+        void decrementBureacrat();
+        class GradeTooHighException: public std::exception
+        {
+            public :
+                const char * what() const throw();
+        };
+        class GradeTooLowException: public std::exception
+        {
+            public :
+                const char * what() const throw();
+        };
+        ~Bureaucrat();
+};
+
+        std::ostream & operator<<(std::ostream &output, const Bureaucrat &obj);
+
+#endif
