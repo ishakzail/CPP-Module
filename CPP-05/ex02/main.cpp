8bc4f99756fc325d1654f24eedcc47410e943ada
ishakzail <ishak.zail.iz@gmail.com>
2022-10-26 18:49:12 +0100

    add tests, need to add more

--- CPP-05/ex02/main.cpp
+++ CPP-05/ex02/main.cpp
@@ -6,37 +6,26 @@
 /*   By: izail <izail@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 20:33:21 by izail             #+#    #+#             */
-/*   Updated: 2022/10/26 10:53:42 by izail            ###   ########.fr       */
+/*   Updated: 2022/10/26 18:08:34 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Bureaucrat.hpp"
+#include "ShrubberyCreationForm.hpp"
 
 int main()
 {
-    
-	Form Form0("Chahada#", 1, 70);
-	Bureaucrat Bureaucrat0("Qaid#", 1);
-
-	std::cout << std::endl;
-	std::cout << Form0 << std::endl;
-	std::cout << Bureaucrat0 << std::endl;
-
-	std::cout << "[ signForm " << Form0.getName() << " ! ]" << std::endl;
-	std::cout << std::endl;
-	Bureaucrat0.signForm(Form0);
+	Bureaucrat B1("Ishak", 160);
+	ShrubberyCreationForm shru("wati9a");
 	try
 	{
-		Form0.beSigned(Bureaucrat0);
-		std::cout << std::endl;
-		std::cout << Form0 << std::endl;
+		shru.beSigned(B1);
+		shru.execute(B1);
 	}
-	catch (std::exception& e)
+	catch(const std::exception& e)
 	{
-		std::cout << e.what() << std::endl;
-		std::cout << std::endl;
-		std::cout << Form0 << std::endl;
+		std::cerr << e.what() << '\n';
 	}
-	std::cout << std::endl;
-
+	
+	
+	
 }
