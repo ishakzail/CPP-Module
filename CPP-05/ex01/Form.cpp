ddf09626c95fb3333419e52bff6f53982bd4059f
ishakzail <ishak.zail.iz@gmail.com>
2022-10-26 11:29:26 +0100

    add param constructor

--- CPP-05/ex01/Form.cpp
+++ CPP-05/ex01/Form.cpp
@@ -6,7 +6,7 @@
 /*   By: izail <izail@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 14:53:38 by izail             #+#    #+#             */
-/*   Updated: 2022/10/25 16:09:48 by izail            ###   ########.fr       */
+/*   Updated: 2022/10/26 10:33:53 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,29 @@
 
 Form::Form() : name("noName"), _signed(false), sign_grade(1), execute_grade(1)
 {
-    std::cout << "Default constructor called" << std::endl;
+    std::cout << "Default constructor called by FORM" << std::endl;
+}
+
+Form::Form(const std::string _name, const unsigned int _sign_grade, const unsigned int _execute_grade) 
+    : name(_name), _signed(false), sign_grade(_sign_grade), execute_grade(_execute_grade)
+{
+    std::cout << "Param constructor called by FORM" << std::endl;
+    try
+    {
+        if (this->sign_grade < 1 || this->execute_grade < 1)
+            throw (GradeTooHighException());
+        else if (this->sign_grade > 150 || this->execute_grade > 150)
+            throw(GradeTooLowException());
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what();
+    }
 }
 
 Form::Form(const Form& obj) :  name(obj.name), sign_grade(obj.sign_grade), execute_grade(obj.execute_grade)  
 {
-    std::cout << "Copy Constructor called" << std::endl;
+    std::cout << "Copy Constructor called by FORM" << std::endl;
     this->_signed = obj._signed;
     *this = obj; 
 }
@@ -39,7 +56,7 @@ std::string Form::getName() const
     return this->name;
 }
 
-bool Form::getSigned()
+bool Form::getSigned() const
 {
     return this->_signed;
 }
@@ -49,7 +66,7 @@ void Form::beSigned(const Bureaucrat &obj)
     if(obj.getGrade() <= this->sign_grade)
         this->_signed = true;
     else
-        throw (GradeTooHighException());       
+        throw (GradeTooLowException());       
 }
 
 const char *Form::GradeTooHighException::what() const throw()
@@ -64,7 +81,7 @@ const char *Form::GradeTooLowException::what() const throw()
 
 Form & Form::operator=(const Form& obj)
 {
-    std::cout << "Copy assignment operator called" << std::endl;
+    std::cout << "Copy assignment operator called by FORM" << std::endl;
     if (this != &obj)
     {
         const_cast<std::string&>(this->name) = obj.name;
@@ -75,7 +92,19 @@ Form & Form::operator=(const Form& obj)
     return (*this);
 }
 
+std::ostream & operator<<(std::ostream &output, const Form &obj)
+{
+    output << "Form name : " << obj.getName() << "\nsign grade " << obj.getSignGrade() << "\nexecute grade " \
+    << obj.getExecuteGrade() << std::endl;
+    output << "is signed : ";
+    if (obj.getSigned())
+        std::cout << "true\n";
+    else
+        std::cout << "false\n";
+    return output; 
+}
+
 Form::~Form()
 {
-    std::cout << "Destructor called" << std::endl;
+    std::cout << "Destructor called by FORM" << std::endl;
 }
