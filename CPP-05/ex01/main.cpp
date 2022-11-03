901b35e632ebb5a7a9b53cb7c16dbe7181411bff
ishakzail <ishak.zail.iz@gmail.com>
2022-11-03 20:39:56 +0100

    update main in ex01 -- CP05

--- CPP-05/ex01/main.cpp
+++ CPP-05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: izail <izail@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 20:33:21 by izail             #+#    #+#             */
-/*   Updated: 2022/10/26 10:53:42 by izail            ###   ########.fr       */
+/*   Updated: 2022/11/02 12:12:45 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 int main()
 {
     
-	Form Form0("Chahada#", 1, 70);
-	Bureaucrat Bureaucrat0("Qaid#", 1);
+	Form Form0("Chahada#", 6, 70);
+	Bureaucrat Bureaucrat0("Qaid#", 8);
 
 	std::cout << std::endl;
 	std::cout << Form0 << std::endl;
@@ -24,18 +24,16 @@ int main()
 
 	std::cout << "[ signForm " << Form0.getName() << " ! ]" << std::endl;
 	std::cout << std::endl;
-	Bureaucrat0.signForm(Form0);
 	try
 	{
-		Form0.beSigned(Bureaucrat0);
+		// Form0.beSigned(Bureaucrat0);
+		// std::cout << Form0 << std::endl;
+		Bureaucrat0.signForm(Form0);
 		std::cout << std::endl;
-		std::cout << Form0 << std::endl;
 	}
 	catch (std::exception& e)
 	{
 		std::cout << e.what() << std::endl;
-		std::cout << std::endl;
-		std::cout << Form0 << std::endl;
 	}
 	std::cout << std::endl;
 
