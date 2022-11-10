c11b9ac020cc7414871494abd35445e4e4975cfb
ishakzail <ishak.zail.iz@gmail.com>
2022-11-10 15:57:23 +0100

    CP07- ex01 "version with class"

--- CPP-07/ex01/Iter.hpp
+++ CPP-07/ex01/Iter.hpp
@@ -6,7 +6,7 @@
 /*   By: izail <izail@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 11:59:27 by izail             #+#    #+#             */
-/*   Updated: 2022/11/09 15:19:45 by izail            ###   ########.fr       */
+/*   Updated: 2022/11/10 15:53:44 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <iostream>
 
 template <typename T> 
-void iter(T *arr, T arrLenght, T (*f)(T&))
+void iter(T *arr, int arrLenght, void (*f)(T&))
 {
     for (int i = 0; i < arrLenght; i++)
     {
@@ -35,11 +35,17 @@ class Iter
     private :
         int nbr;
     public :
-        Iter() : nbr(0){}
-        Iter(int _nbr) : nbr(_nbr) {}
-        
+        Iter() : nbr(8){std::cout <<"Default const";}
+        Iter(int _nbr) : nbr(_nbr) {std::cout <<"Param const";}
+        int getNbr() { return nbr;}
         ~Iter(){}
     
 };
+
+std::ostream& operator<<(std::ostream& out , Iter& var)
+{
+	out << var.getNbr();
+	return out;
+}
 
 #endif
